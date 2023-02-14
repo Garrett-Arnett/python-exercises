951152583689c3b8d1a6cab5037ea1a6ce393b53
“Garrett <garrett.paul.arnett@gmail.com>
2023-02-14 10:36:33 -0600

    ~~~  Notes ~~~
    # This code is able to run numerical arguments up to and no more than 2 decimal places,
    # for example you can deposit $20.24 and a credit of $20.24 will be added to your account.
    
    # The user cannnot debit or credit a negative number into the system. If the user tries to, the
    # transation will be cancelled and the interaction will reset.
    
    # The user cannot have a balance that will fall under 0. If the user withdraws an amount exceeding
    # the users current balance, the transaction will be canceled and the interaction will reset. WORKING EDIT WITH NO KNOWN BUGS as of 2/14/2023 10AM

--- python_checkbook_project.py
+++ python_checkbook_project.py
@@ -1,87 +1,16 @@
+#                          ~~~  Notes ~~~
+
+# This code is able to run numerical arguments up to and no more than 2 decimal places, 
+# for example you can deposit $20.24 and a credit of $20.24 will be added to your account.
+
+# The user cannnot debit or credit a negative number into the system. If the user tries to, the 
+# transation will be cancelled and the interaction will reset.
 
 
-'''Command Line Checkbook Application
-You will create a command line checkbook application that allows 
-users to track their finances with a command line interface.
-
-Project Requirements
-Build a .py file that will be run from the command line.
-
-When run, the application should welcome the user, and prompt them
-for an action to take:
-
-view current balance
-add a debit (withdrawal)
-add a credit (deposit)
-exit
-
-The application should notify the user if the input is invalid and
-prompt for another choice.
-
-The application should persist between times that it is run.
-
-Example, if you run the application, add some credits, exit the 
-application and run it again, you should still see the balance that 
-you previously created. In order to do this, your application will 
-need to store its data in a text file. Consider creating a file where 
-each line in the file represents a single transaction.
-
-Utilize functions to call the balance, debit, credit, and exit
-
-Example Ouput
-Here is an example of what using the program might look like:
+# The user cannot have a balance that will fall under 0. If the user withdraws an amount exceeding 
+# the users current balance, the transaction will be canceled and the interaction will reset.
 
 
-    $ python checkbook.py
-
-    ~~~ Welcome to your terminal checkbook! ~~~
-
-    What would you like to do?
-
-    1) view current balance
-    2) record a debit (withdraw)
-    3) record a credit (deposit)
-    4) exit
-
-    Your choice? 5
-    Invalid choice: 5
-
-    Your choice? 1
-
-    Your current balance is $100.00.
-
-    What would you like to do?
-
-    1) view current balance
-    2) record a debit (withdraw)
-    3) record a credit (deposit)
-    4) exit
-
-    Your choice? 2
-
-    How much is the debit? $50
-
-    What would you like to do?
-
-    1) view current balance
-    2) record a debit (withdraw)
-    3) record a credit (deposit)
-    4) exit
-
-    Your choice? 1
-
-    Your current balance is $50.00.
-
-    What would you like to do?
-
-    1) view current balance
-    2) record a debit (withdraw)
-    3) record a credit (deposit)
-    4) exit
-
-    Your choice? 4
-
-    Thanks, have a great day! '''
 
 import os
 
@@ -100,34 +29,58 @@ def view_balance():
             balance += float(line)
         return print('Your current balance is ', '${:.2f}'.format(balance))
 
+def get_valid_float(amount):
+    while True:
+        try:
+            value = float(input(amount))
+            if value != round(value, 2):
+                print("Please enter a value with no more than two decimal places.")
+            else:
+                return value
+        except ValueError:
+            print("Please enter a valid number.")
+    
 def withdraw(amount):
+    if amount < 0:
+        print("Error: withdraw amount must be a positive number. Transaction cancelled.")
+        return
     check_file()
-    with open(transactions_file, "a") as f:
-        f.write(str(-amount) + "\n")
-
+    with open(transactions_file, "r") as f:
+        lines = f.readlines()
+        balance = 0.0
+        for line in lines:
+            balance += float(line)
+        if balance - amount < 0:
+            print("Withdrawal amount is greater than current balance. Transaction cancelled.")
+        elif balance - amount >= 0:
+            with open(transactions_file, "a") as f:
+                f.write(str(-amount) + "\n")
+                print('Debit of', '${:.2f}'.format(amount), 'is successful!')
 def deposit(amount):
+    if amount < 0:
+        print("Error: deposit amount must be a positive number. Transaction cancelled.")
+        return
     check_file()
     with open(transactions_file, "a") as f:
         f.write(str(amount) + "\n")
-
+    print('Credit of ' '${:.2f}'.format(amount) ,'is successful!')
+print('~~~ Welcome to your terminal checkbook! ~~~')
 while True:
     print('What would you like to do?\n\n1) View current balance\n2) Record a debit (Withdraw)\n3) Record a credit (Deposit)\n4) Exit')
-    num = int(input('Your Choice? '))
+    num = input('Your Choice? ')
 
-    if num == 1:
+    if num == '1':
         view_balance()
         continue
-    elif num == 2:
-        amount = float(input('Enter the amount of the debit: '))
+    elif num == '2':
+        amount = get_valid_float('Enter the amount of the debit: ')
         withdraw(amount)
-        print(('Debit of', '${:.2f}'.format(amount), 'is successful!'))
         continue
-    elif num == 3:
-        amount = float(input('Enter the amount of the withdraw: '))
+    elif num == '3':
+        amount = get_valid_float('Enter the amount of the credit: ')
         deposit(amount)
-        print('Credit of', '${:.2f}'.format(amount),' is successful!')
         continue
-    elif num == 4:
+    elif num == '4':
         print('Thank you, goodbye!')
         break
     else:
